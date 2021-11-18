7e9d982b8dc6ccf3074f0f85893a45097c9e5cc5
CVBertrandCardon <bertrandcardondel@gmail.com>
2021-11-18 19:37:23 +0100

    first commit

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DisplayPosts from '../components/DisplayPosts';
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <DisplayPosts/>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/components/DisplayPosts.tsx
@@ -0,0 +1,88 @@
+
+import React, { useEffect, useState } from 'react';
+
+export interface Post {
+        userId:number,
+        id: number,
+        title: string,
+        body:string,
+        completed:boolean
+    }
+
+    export interface Comment {
+        postId:number,
+        id: number,
+        name: string,
+        email:string,
+        body:string
+    }
+    
+    
+    const DisplayPosts  = () => {
+        
+        
+        const [posts, setPosts] = useState<Post[]>([])
+        const [comments, setComments] =useState<Comment[]>([])
+
+    
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts/')
+        .then(response => response.json())
+        .then(res => {  
+            setPosts(res)
+        } )
+        fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(response => response.json())
+        .then(res => {  
+            setComments(res)
+        } )
+    },[])
+    
+    return (
+        <div className="display-posts">
+
+<ul className="collection">
+                        {posts.map(post =>
+                            (
+                                <div>
+                                    <div
+                                        key={post.id}
+                                        className="posts-container"
+                                    >
+                                        <h2>{post.title}</h2>
+                                        <p>{post.body}</p>
+                                    </div>
+                                    <div className="jumbotron-div col s12">
+                                        <ul className="collection">
+                                        {
+                                            comments.map(comment => {
+                                                if (post.id === comment.postId) {
+
+                                                   return (
+                                                        <li
+                                                            key={comment.id}
+                                                            className="collection-item left-align purple lighten-2"
+                                                        >
+                                                            <p className="email">{comment.email} </p>
+                                                            <p>{comment.body}</p>
+                                                        </li>
+                                                    )
+                                                } else {
+                                                    return ""
+                                                }
+                                            }
+                                                )
+                                            }
+                                        </ul>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </ul>
+            
+            
+        </div>
+    );
+};
+
+export default DisplayPosts;
