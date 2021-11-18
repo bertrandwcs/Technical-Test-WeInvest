f0e37e5f77efdc012a6786f8513f6bdc07267402
CVBertrandCardon <bertrandcardondel@gmail.com>
2021-11-18 22:28:30 +0100

    part of the second user story and Third complete

--- src/components/DisplayPosts.tsx
+++ src/components/DisplayPosts.tsx
@@ -1,5 +1,8 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import CopyToClipboard from "react-copy-to-clipboard";
+
+/* Create interface for data's type*/
 
 export interface Post {
         userId:number,
@@ -21,28 +24,44 @@ export interface Post {
     const DisplayPosts  = () => {
         
         
-        const [posts, setPosts] = useState<Post[]>([])
-        const [comments, setComments] =useState<Comment[]>([])
+        const [posts, setPosts] = useState<Post[]>([]);
+        const [comments, setComments] =useState<Comment[]>([]);
 
-    
-    useEffect(() => {
+/* Sort by id's and most commented */
+        const [sortId, setSortId] = useState(true);
+        const [sortComment, setSortComment] = useState(true);
+        
+
+/* fetch data's */
+
         fetch('https://jsonplaceholder.typicode.com/posts/')
         .then(response => response.json())
         .then(res => {  
             setPosts(res)
-        } )
+        } );
         fetch('https://jsonplaceholder.typicode.com/comments')
         .then(response => response.json())
         .then(res => {  
             setComments(res)
-        } )
-    },[])
+        } );
+
+        const handleSortById = () => {
+            setSortId(!sortId);
+            console.log(sortId)
+        }
+        const handleSortByMostCommented = () => {
+            setSortComment(!sortComment);
+        }
+
     
     return (
         <div className="display-posts">
-
-<ul className="collection">
-                        {posts.map(post =>
+            <div className="container-button">
+                <button onClick={handleSortById}>Sort by id's</button>
+                <button onClick={handleSortByMostCommented}>Sort by most commented</button>
+            </div>
+            <ul className="collection">
+                        {posts.sort(sortId?(a, b) => a.id - b.id:(a, b) => b.id - a.id).map(post =>
                             (
                                 <div>
                                     <div
@@ -50,6 +69,22 @@ export interface Post {
                                         className="posts-container"
                                     >
                                         <h2>{post.title}</h2>
+                                        <div className="dropdown">
+                                            <span>Post Menu</span>
+                                            <div className="dropdown-content">
+                                        <CopyToClipboard text={`${post.id}`}>
+                                         <p onClick={() => {
+                                            post.id?alert(`Id: ${post.id} copy to clipboard`):alert("No ID for this post!");
+                                         }}>post id: {post.id}</p>  
+                                         </CopyToClipboard>
+                                            <p onClick={() => {
+                                                fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}`, {
+                                                    method: 'DELETE',
+                                                  });
+                                                alert("Post deleted !")
+                                            }}>Delete post</p>
+                                            </div>
+                                        </div>
                                         <p>{post.body}</p>
                                     </div>
                                     <div >
@@ -57,7 +92,6 @@ export interface Post {
                                         {
                                             comments.map(comment => {
                                                 if (post.id === comment.postId) {
-
                                                    return (
                                                         <li
                                                             key={comment.id}
